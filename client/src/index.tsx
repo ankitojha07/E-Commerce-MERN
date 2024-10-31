928582e6405cb13c65457e3fb7972bc748df853a
ankitojha07 <ankitojha1409@gmail.com>
2024-10-31 16:16:09 +0530

    Fixed cart controller & moved to production

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://e-commerce-mern-coral.vercel.app/";
-axios.defaults.baseURL = "http://localhost:5000/";
+axios.defaults.baseURL = "https://e-commerce-mern-coral.vercel.app/";
+// axios.defaults.baseURL = "http://localhost:5000/";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
